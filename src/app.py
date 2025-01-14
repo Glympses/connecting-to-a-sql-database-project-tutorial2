e92a1ad1e96af8bf04d20919051bab8b0b8e0140
Mario Vilches <110475661+mfvilches@users.noreply.github.com>
2025-01-14 01:33:03 +0000

    clase

--- src/app.py
+++ src/app.py
@@ -8,10 +8,15 @@ load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
 
-connection_string = f"postgresql://{os.getenv('gitpod')}:{os.getenv('postgres')}@{os.getenv('localhost')}/{os.getenv('sample_db')}"
+connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 engine = create_engine(connection_string).execution_options(autocommit=True)
-engine.connect()
+conn = engine.connect()
 
+engine.execute(""" DROP TABLE book_authors;
+               DROP TABLE books;
+               DROP TABLE authors;
+               DROP TABLE publishers;
+               """)
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 engine.execute(""" CREATE TABLE publishers(
     publisher_id INT NOT NULL,
@@ -94,5 +99,5 @@ INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);   """)
 
 
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
-dataframe = pd.read_sql("Select * from books;", engine)
+dataframe = pd.read_sql("Select * from books;", conn)
 print(dataframe.describe())
